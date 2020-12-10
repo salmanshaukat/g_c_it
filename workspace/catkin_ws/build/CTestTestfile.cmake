64ff028648a837fd9950ff60037da3563400a237
salmanshaukat <salmanalishoukat@gmail.com>
2020-12-10 11:09:26 +0400

    first problem comit in 5 tab of project

---
+++ workspace/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/s/Downloads/go_chase_it/workspace/catkin_ws/src
+# Build directory: /home/s/Downloads/go_chase_it/workspace/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(my_robot)
